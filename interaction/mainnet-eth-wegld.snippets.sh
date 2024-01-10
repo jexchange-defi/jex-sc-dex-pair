f553e21d098dd73ecad9dd6d0cab6ce5619f95bf
Aurélien Passion <aurelien.passion@gmail.com>
2024-01-10 09:58:00 +0100

    add interaction script for ETH-WEGLD LP

---
+++ interaction/mainnet-eth-wegld.snippets.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BYTECODE=../output-docker/jex-sc-dex-pair/jex-sc-dex-pair.wasm
+PROXY=https://gateway.multiversx.com
+SC_ADDRESS=$(mxpy data load --key=address-mainnet-eth-egld)
+CHAIN=1
+SCRIPT_DIR=$(dirname $0)
+FIRST_TOKEN_ID=WETH-b4ca29
+SECOND_TOKEN_ID=WEGLD-bd4d79
+
+source "${SCRIPT_DIR}/_common.snippets.sh"
+
+# Reproducible build using:
+# mxpy contract reproducible-build --docker-image="multiversx/sdk-rust-contract-builder:v5.3.0"
+deploy() {
+    echo 'You are about to deploy SC on mainnet (Ctrl-C to abort)'
+    read answer
+
+    mxpy contract deploy --bytecode=${BYTECODE} --metadata-not-upgradeable \
+         --keyfile=${1} --gas-limit=80000000 --outfile="deploy-mainnet.interaction.json" \
+         --arguments "str:${FIRST_TOKEN_ID}" "str:${SECOND_TOKEN_ID}" \
+         --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
+
+    SC_ADDRESS=$(cat deploy-mainnet.interaction.json | jq -r .contractAddress)
+
+    mxpy data store --key=address-mainnet-eth-egld --value=${SC_ADDRESS}
+
+    echo ""
+    echo "Smart contract address: ${SC_ADDRESS}"
+}
+
+upgrade() {
+    echo 'You are about to upgrade current SC on mainnet (Ctrl-C to abort)'
+    read answer
+
+    mxpy contract upgrade --bytecode=${BYTECODE} --metadata-not-upgradeable \
+        --keyfile=${1} --gas-limit=80000000 --outfile="deploy-mainnet.interaction.json" \
+        --arguments "0x" "0x" \
+        --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send ${SC_ADDRESS} || return
+
+    echo ""
+    echo "Smart contract upgraded: ${SC_ADDRESS}"
+}
+
+verify() {
+    mxpy contract verify "${SC_ADDRESS}" \
+        --packaged-src=../output-docker/jex-sc-dex-pair/jex-sc-dex-pair-0.0.0.source.json \
+        --verifier-url="https://play-api.multiversx.com" \
+        --docker-image="multiversx/sdk-rust-contract-builder:v5.3.0" \
+        --keyfile=${1}
+}
+
+CMD=$1
+shift
+
+$CMD $*
